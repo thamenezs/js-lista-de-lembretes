ff2d144c90a82924e5b32d52a721092e2aa205d7
Thais Menezes <thais.mas@hotmail.com>
2023-01-04 20:40:54 -0300

    new commit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@ let novoLembrete = document.querySelector('#novoLembrete');
 let novoLembreteData = document.querySelector('#novoLembreteData');
 let buttonCriar = document.querySelector('#buttonCriar');
 let listaTarefas = document.querySelector('#listaTarefas');
+let dl = document.querySelector('#testeNovo');
 let dbLembretes = [];
-
-
 
 
 buttonCriar.addEventListener('click', (e) => {
@@ -21,63 +20,79 @@ buttonCriar.addEventListener('click', (e) => {
 });
 
 
+function criarLembrete(lembrete) {
+    dbLembretes.push(lembrete);
+    localStorage.setItem('listaTarefas', JSON.stringify(dbLembretes));
+
+    dbLembretes.sort((a, b) => new Date(a.data) - new Date(b.data));
+    limparFilhos()
+    for(let i = 0 ; i < dbLembretes.length ; i++) {
+        let dl = tagDl(dbLembretes[i]);
+        listaTarefas.appendChild(dl);
+    }
+    
+    novoLembrete.value = '';
+    novoLembreteData.value = '';
+
+}
+
 function gerarId() {
     return Math.floor(Math.random() * 3000);
 }
 
-function criarLembrete(lembrete) {
-    dbLembretes.push(lembrete);
-    localStorage.setItem('listaTarefas', JSON.stringify(dbLembretes));
-    let li = tagLi(lembrete);
-    
-    listaTarefas.appendChild(li);
-    novoLembrete.value = '';
-    novoLembreteData.value = '';
-    
-
+function limparFilhos() {
+    dl.innerText = ""
 }
 
-
-function tagLi(lembrete) {
-
-    let li = document.createElement('li');
-
-    li.id = lembrete.id;
-
-    let date = document.createElement('date');
-    date.classList.add('dataTarefa');
-    date.innerHTML = lembrete.data;
-
-    let span = document.createElement('span');
-    span.classList.add('textoTarefa');
-    span.innerHTML = lembrete.nome;
-
-   
+function tagDl(lembrete) {
     
-    let div = document.createElement('div');
+    let date = new Date(lembrete.data);
+    idDate = date.getTime();
+    idDate = String(idDate);
 
+
+    let dt = document.getElementById(idDate);
+
+    if (dt == null) {
+        // o elemento não existe
+        dt = document.createElement('dt');
+        dt.id = (idDate); 
+        dt.textContent = lembrete.data;
+        dl.appendChild(dt);
+      }
+
+    let dd = document.createElement('dd');
+    dd.textContent = lembrete.nome;
+    dd.id = lembrete.id;
+    
     let btn = document.createElement('btn');
     btn.classList.add('btn-delete');
     btn.innerHTML = '<i class="fa fa-trash"></i>';
     btn.setAttribute('onclick', 'excluir('+lembrete.id+')');
 
-    div.appendChild(btn);
-    date.appendChild(li);
-    li.appendChild(span);
     
-    li.appendChild(div);
+    dt.appendChild(dd);
+    dd.appendChild(btn);
 
-    return date;
-    return li;
+    return dl;
 
 }
 
+
 function excluir(idLembrete) {
     let confirmacao = window.confirm('Tem certeza que deseja excluir o lembrete?');
-    if(confirmacao){
-        let li = document.getElementById(''+ idLembrete + '');
-        if(li){
-            listaTarefas.removeChild(li);
+    if(confirmacao) {
+        let elemento = dbLembretes.find(item => item.id === idLembrete);
+        dbLembretes.splice(dbLembretes.indexOf(elemento), 1);
+        let dd = document.getElementById(''+ idLembrete + '');
+        let dt = dd.parentNode;
+        var regExp = /[a-zA-Z]/g;
+
+        if(dd){
+            dd.remove();
+        }
+        if(!regExp.test(dt.textContent)){ 
+            dt.remove()
         }
     }
 }
@@ -91,6 +106,7 @@ function validaNome(nomeLembrete){
 }
 
 function validaData(dataLembrete){
+    console.log(dataLembrete);
     const localDate = new Date();
     const insertDate = new Date(dataLembrete);
     insertDate.setDate(insertDate.getDate() + 1);
@@ -98,7 +114,7 @@ function validaData(dataLembrete){
     if(dataLembrete == "" || dataLembrete == null ) {
         alert('Preencha corretamente a data da tarefa!');
         return false;
-    } else if(localDate.toLocaleDateString() > insertDate.toLocaleDateString()) {
+    } else if(localDate.getTime() > insertDate.getTime()) {
         alert('Insira uma data futura');
         return false;
     } else return true;
